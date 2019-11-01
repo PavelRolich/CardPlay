9eb719c1326e0540749529470431d54abd11bb73
Pavel <pahan.rolich@mail.ru>
2019-11-01 16:30:58 +0300

    create component

--- Game/src/app/app.module.ts
+++ Game/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
